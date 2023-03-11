3e8aaf99580e7436d3746df79cd966587a28e779
shourya-r <112893395+shourya-r@users.noreply.github.com>
2023-03-11 20:49:05 +0530

    Add files via upload

---
+++ Pattern 3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/* PRINT PATTERN 
+    1 1 1
+    2 2 2
+    3 3 3  */
+
+int main() {
+
+    int n ;
+    cin>>n;
+    for (int i = 1; i<=n; i++) {
+        
+        for(int j = 1; j<=n; j++) {
+            cout<< i<<" ";
+        }
+    }
+
+}
