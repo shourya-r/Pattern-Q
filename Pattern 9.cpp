12f61202a712b2d7ef6a8add1c4fa3bd828e86e0
shourya-r <112893395+shourya-r@users.noreply.github.com>
2023-03-12 16:06:45 +0530

    Add files via upload

---
+++ Pattern 9.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+/* PRINT PATTERN 
+    * * * * 
+    * * *
+    * *
+    *        */
+
+int main() {
+
+    int n ;
+    cin>>n;
+    
+    for (int i = n ; i>=1 ; i--) {
+        
+        for(int j = i; j>=1; j--) {
+            cout<<"*"<<" ";
+        }
+    cout<<endl;
+    }
+
+}
