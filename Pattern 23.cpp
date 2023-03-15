9120cebd70192174c5b0b514ebe4f0a8617eeacc
shourya-r <112893395+shourya-r@users.noreply.github.com>
2023-03-15 22:25:01 +0530

    Add files via upload

---
+++ Pattern 23.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+/* PRINT SQUARE PATTERN 
+        *****
+        *   *
+        *   *
+        *   *
+        *****    */
+
+int main() {
+    
+    int n ;
+    cin>>n;
+    
+    for (int i = 1 ; i<=n ; i++) {
+
+        for (int j = 1; j<=n ; j++) {
+            if (i==1 || j==1 || i==n || j==n){
+                cout<<"*";
+            }
+            else {
+                cout<<" ";
+            }
+        }   
+
+    cout<<endl;
+    }
+
+}
