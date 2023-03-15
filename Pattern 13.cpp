a07e2315be635b0a1e6c9a9814abd2f50ec4ce1d
shourya-r <112893395+shourya-r@users.noreply.github.com>
2023-03-15 15:37:02 +0530

    Add files via upload

---
+++ Pattern 13.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+/* PRINT PATTERN 
+            *
+           ***
+          *****
+         ******* 
+         *******
+          *****
+           ***
+            *    */
+
+int main() {
+    
+    int n ;
+    cin>>n;
+    
+    for (int i = 1 ; i<=n ; i++) {
+        // MAKE THREE INNER LOOPS FOR THE SPACES, STARS AND THEN SPACE AND RELATE THE NUMBER OF SPACES AND STARS WITH THE VARIABLES
+        for(int j = 1; j<=(n-i); j++) {  // SPACES
+            cout<<" ";
+        }
+        for(int k = 1; k<=(2*i-1); k++) {  // STARS
+            cout<<"*";
+        }
+        for(int l = 1; l<=(n-i); l++) {  // SPACES
+            cout<<" ";
+        }
+    cout<<endl;
+    }
+    for (int i = 1 ; i<=n ; i++) {
+        for(int j = 0; j<(i-1); j++) {  // SPACES
+            cout<<" ";
+        }
+        for(int k = 1; k<=(2*n-2*i+1); k++) {  // STARS
+            cout<<"*";
+        }
+        for(int l = 0; l<(i-1); l++) {  // SPACES
+            cout<<" ";
+        }
+    cout<<endl;
+    }
+}
